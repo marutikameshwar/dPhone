e98afefb1ce4d9c43ceef4e63cd9aad61cefed59
Jaideep Varanasi <54927461+dattajaideep@users.noreply.github.com>
2023-02-15 11:50:54 +0530

    annotations fixes

--- src/main/java/com/bajaj/entity/OrdersEntity.java
+++ src/main/java/com/bajaj/entity/OrdersEntity.java
@@ -23,5 +23,83 @@ public class OrdersEntity {
     private String orderStatus;
     public OrdersEntity() {
     }
-   
+
+    public OrdersEntity(int orderId, int userId, int productId, String orderDate, int redeemedPoints, int salePrice, String orderStatus) {
+        this.orderId = orderId;
+        this.userId = userId;
+        this.productId = productId;
+        this.orderDate = orderDate;
+        this.redeemedPoints = redeemedPoints;
+        this.salePrice = salePrice;
+        this.orderStatus = orderStatus;
+    }
+
+    public int getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(int orderId) {
+        this.orderId = orderId;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public int getProductId() {
+        return productId;
+    }
+
+    public void setProductId(int productId) {
+        this.productId = productId;
+    }
+
+    public String getOrderDate() {
+        return orderDate;
+    }
+
+    public void setOrderDate(String orderDate) {
+        this.orderDate = orderDate;
+    }
+
+    public int getRedeemedPoints() {
+        return redeemedPoints;
+    }
+
+    public void setRedeemedPoints(int redeemedPoints) {
+        this.redeemedPoints = redeemedPoints;
+    }
+
+    public int getSalePrice() {
+        return salePrice;
+    }
+
+    public void setSalePrice(int salePrice) {
+        this.salePrice = salePrice;
+    }
+
+    public String getOrderStatus() {
+        return orderStatus;
+    }
+
+    public void setOrderStatus(String orderStatus) {
+        this.orderStatus = orderStatus;
+    }
+
+    @Override
+    public String toString() {
+        return "OrdersEntity{" +
+                "orderId=" + orderId +
+                ", userId=" + userId +
+                ", productId=" + productId +
+                ", orderDate='" + orderDate + '\'' +
+                ", redeemedPoints=" + redeemedPoints +
+                ", salePrice=" + salePrice +
+                ", orderStatus='" + orderStatus + '\'' +
+                '}';
+    }
 }
