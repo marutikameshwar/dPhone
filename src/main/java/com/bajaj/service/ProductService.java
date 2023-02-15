19b0e646ad63b83d9aa6e21c639260500a823e47
Appani Kaushik <appanikaushik@L-YN4LWQHP4F.local>
2023-02-15 11:44:11 +0530

    new

--- src/main/java/com/bajaj/service/ProductService.java
+++ src/main/java/com/bajaj/service/ProductService.java
@@ -5,8 +5,7 @@ import java.util.List;
 
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+
 import com.bajaj.beans.ProductBean;
 import com.bajaj.entity.ProductEntity;
 
